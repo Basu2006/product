0494930d2b235b6f9a49bb7b81963333ff867c45
Basu2006 <basavarajholal26@gmail.com>
2025-12-11 15:49:34 +0530

    save

---
+++ test_product.py
@@ -0,0 +1,17 @@
+def product_details(product_id, name, quantity, price):
+    result = (
+        f"Product ID: {product_id}\n"
+        f"Product Name: {name}\n"
+        f"Quantity: {quantity}\n"
+        f"Price: {price}"
+    )
+    return result
+
+
+if __name__ == "__main__":
+    product_id = "P101"
+    name = "Mobile"
+    quantity = 5
+    price = 45000
+
+    print(product_details(product_id, name, quantity, price))
